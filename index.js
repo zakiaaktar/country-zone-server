560be7641771ab4f84607f1674021dacb6dee487
Zakia Aktar <zakiaaktar@Zakias-MacBook-Pro.local>
2023-02-25 23:24:56 +0100

    job assignment

---
+++ index.js
@@ -0,0 +1,103 @@
+const express = require('express')
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+
+// middle wares
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xwlx9fx.mongodb.net/?retryWrites=true&w=majority`;
+//console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+async function run() {
+  try {
+    const countryCollection = client.db('restCountry').collection('countries');
+
+
+
+
+    app.get('/countries', async (req, res) => {
+
+      // const search = req.query.search;
+      // console.log(search);
+
+      const query = {};
+      // let query = {};
+      // if(search.length){
+
+      // }
+      // const query = {area: { $lt: 65300}}
+      // const query = {region: { $in: [Oceania]}}
+      const order = req.query.order === 'asc' ? 1 : -1;
+      const cursor = countryCollection.find(query).sort({ name: order });
+      const countries = await cursor.toArray();
+      // const countries = await cursor.limit(10).toArray();
+      res.send(countries);
+      // const count = await countryCollection.estimatedDocumentCount();
+      // res.send({count, countries});
+
+
+    });
+
+
+
+
+//     app.get('/pagination', async (req, res) => {
+//       const query = {};
+//       const cursor = countryCollection.find(query)
+//       const countries = await cursor.toArray();
+//       // const countries = await cursor.limit(10).toArray();
+//       const count = await countryCollection.estimatedDocumentCount();
+//       res.send({count, countries});
+// });
+
+
+
+
+
+    app.get('/SmallerLithuania', async (req, res) => {
+      // const query = {};
+      const query = { area: { $lt: 65300 } }
+      // const query = {region: { $in: [Oceania]}}
+      const order = req.query.order === 'asc' ? 1 : -1;
+      const cursor = countryCollection.find(query).sort({ name: order });
+      const countries = await cursor.toArray();
+      // const countries = await cursor.limit(10).toArray();
+      res.send(countries);
+      // const count = await countryCollection.estimatedDocumentCount();
+      // res.send({count, countries});
+
+
+    });
+
+
+  }
+  finally {
+
+  }
+}
+
+run().catch(err => console.error(err));
+
+
+
+app.get('/', (req, res) => {
+  res.send('countries server is running');
+});
+
+app.listen(port, () => {
+  console.log(`countries server running on ${port}`);
+})
